392c95e8ab05df5f727ad2b855575562b9e817e7
SajaLahaleeh <yusra.lahaleeh@gmail.com>
2025-11-02 00:39:28 +0300

    Finish the snapshot

--- src/main/java/com/example/firstImplementation/FirstApproachClass.java
+++ src/main/java/com/example/firstImplementation/FirstApproachClass.java
@@ -2,12 +2,11 @@ package com.example.firstImplementation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication(scanBasePackages = "com.example.firstImplementation")
+@EnableScheduling
 public class FirstApproachClass {
     public static void main(String[] args) {
         SpringApplication.run(FirstApproachClass.class, args);
---
+++ src/main/java/com/example/firstImplementation/service/MetricsPersistenceService.java
@@ -0,0 +1,35 @@
+package com.example.firstImplementation.service;
+
+import java.util.Map;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.example.firstImplementation.model.AggregatedMetrics;
+
+@Service
+public class MetricsPersistenceService {
+
+    private final MetricsAggregator aggregator;
+
+    public  MetricsPersistenceService(MetricsAggregator aggregator){
+        this.aggregator = aggregator;
+    }
+
+// run every 15 min to flush aggregated metrics to DB 
+// 15*60*1000 = 900000 ms
+@Scheduled(fixedRate = 900000)
+public void flushAggregatedMetricsToDB(){
+    Map<String, AggregatedMetrics> snapshot = aggregator.getAll();
+
+    snapshot.forEach((serverId,metrics)-> {
+        System.out.printf("Saving serverId=%s | avgCPU=%.2f | avgMem=%.2f%n" ,
+        serverId,
+        metrics.getAverageCpuUsage(),
+        metrics.getAverageMemoryUsage()
+        );
+        //For now, just print the data (Showq will store it in DB later)
+    });
+}
+
+}
